vividsnow/perl5-data-bloomfilter-shared

Pre-release fixes and documentation corrections

Shared.xs
@@ -10,6 +10,7 @@
         croak("Expected a Data::BloomFilter::Shared object"); \
     BfHandle *h = INT2PTR(BfHandle*, SvIV(SvRV(sv))); \
     if (!h) croak("Attempted to use a destroyed Data::BloomFilter::Shared object"); \
+    BfHandle *h0 = h; PERL_UNUSED_VAR(h0); \
     sv_2mortal(SvREFCNT_inc(SvRV(sv)))
 
 /* Re-read the handle after a call that can run Perl code (tied/overloaded
@@ -19,14 +20,24 @@
  * explicit DESTROY, so the local `h` would dangle.  Used only where magic
  * can actually intervene between EXTRACT and the first use of h. */
 #define REEXTRACT(sv) \
+    if (!SvROK(sv)) \
+        croak("Data::BloomFilter::Shared object was replaced during the call"); \
     h = INT2PTR(BfHandle*, SvIV(SvRV(sv))); \
-    if (!h) croak("Data::BloomFilter::Shared object destroyed during the call")
+    if (h != h0) croak("Data::BloomFilter::Shared object replaced or destroyed during the call")
 
 #define MAKE_OBJ(class, handle) \
     SV *obj = newSViv(PTR2IV(handle)); \
     SV *ref = newRV_noinc(obj); \
     sv_bless(ref, gv_stashpv(class, GV_ADD)); \
     RETVAL = ref
+
+/* Re-read the class-name PV immediately before blessing the new object.
+ * `class` is captured by the typemap in the INPUT section; a tied/overloaded
+ * later constructor argument can run get-magic that reallocs or frees ST(0)'s
+ * PV, dangling that pointer before it is used to bless.  Same fix as
+ * Data::CuckooFilter::Shared already carries. */
+#define REREAD_CLASS() \
+    class = SvPV_nolen(ST(0))
 
 MODULE = Data::BloomFilter::Shared  PACKAGE = Data::BloomFilter::Shared
 
@@ -53,7 +64,8 @@ new(class, path = &PL_sv_undef, capacity = 0, fp_rate = 0.01, ...)
         croak("Data::BloomFilter::Shared->new: fp_rate must be between 0 and 1 (exclusive)");
     const char *p = (SvGETMAGIC(path), SvOK(path)) ? SvPV_nolen(path) : NULL;   /* captured LAST: no magic runs before bf_create uses it */
     BfHandle *h = bf_create(p, (uint64_t)capacity, fp_rate, mode, errbuf);
-    if (!h) croak("Data::BloomFilter::Shared->new: %s", errbuf);
+    if (!h) croak("Data::BloomFilter::Shared->new: %s", errbuf[0] ? errbuf : "out of memory");
+    REREAD_CLASS();
     MAKE_OBJ(class, h);
   OUTPUT:
     RETVAL
@@ -73,7 +85,8 @@ new_memfd(class, name = &PL_sv_undef, capacity = 0, fp_rate = 0.01)
     if (!(fp_rate > 0.0 && fp_rate < 1.0))
         croak("Data::BloomFilter::Shared->new_memfd: fp_rate must be between 0 and 1 (exclusive)");
     BfHandle *h = bf_create_memfd(nm, (uint64_t)capacity, fp_rate, errbuf);
-    if (!h) croak("Data::BloomFilter::Shared->new_memfd: %s", errbuf);
+    if (!h) croak("Data::BloomFilter::Shared->new_memfd: %s", errbuf[0] ? errbuf : "out of memory");
+    REREAD_CLASS();
     MAKE_OBJ(class, h);
   OUTPUT:
     RETVAL
@@ -86,7 +99,29 @@ new_from_fd(class, fd)
     char errbuf[BF_ERR_BUFLEN];
   CODE:
     BfHandle *h = bf_open_fd(fd, errbuf);
-    if (!h) croak("Data::BloomFilter::Shared->new_from_fd: %s", errbuf);
+    if (!h) croak("Data::BloomFilter::Shared->new_from_fd: %s", errbuf[0] ? errbuf : "out of memory");
+    REREAD_CLASS();
+    MAKE_OBJ(class, h);
+  OUTPUT:
+    RETVAL
+
+SV *
+new_readonly(class, path)
+    const char *class
+    SV *path
+  PREINIT:
+    char errbuf[BF_ERR_BUFLEN];
+  CODE:
+    /* Open a FROZEN (sealed) file read-only: O_RDONLY + PROT_READ, no lock
+       ever.  A sealed file is immutable and no read path writes the mapping,
+       so queries take no reader-slot / rwlock traffic and any number of
+       processes can share one PROT_READ mapping (same architecture; the
+       magic rejects a wrong-endian file). */
+    const char *p = (SvGETMAGIC(path), SvOK(path)) ? SvPV_nolen(path) : NULL;
+    if (!p) croak("Data::BloomFilter::Shared->new_readonly: path is required");
+    BfHandle *h = bf_open_readonly(p, errbuf);
+    if (!h) croak("Data::BloomFilter::Shared->new_readonly: %s", errbuf);
+    REREAD_CLASS();
     MAKE_OBJ(class, h);
   OUTPUT:
     RETVAL
@@ -109,9 +144,11 @@ add(self, item)
     STRLEN n;
     const char *s;
   CODE:
+    if (h->readonly) croak("Data::BloomFilter::Shared->add: filter is frozen (read-only)");
     s = SvPVbyte(item, n);                 /* may croak (wide char) -- BEFORE the lock */
     REEXTRACT(self);
     bf_rwlock_wrlock(h);
+    if (h->hdr->sealed) { bf_rwlock_wrunlock(h); croak("Data::BloomFilter::Shared->add: filter is frozen (read-only)"); }
     RETVAL = bf_add_locked(h, s, n);
     __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
     bf_rwlock_wrunlock(h);
@@ -128,10 +165,12 @@ add_many(self, items)
     IV  top;
     UV  added = 0;
   CODE:
+    if (h->readonly) croak("Data::BloomFilter::Shared->add_many: filter is frozen (read-only)");
     SvGETMAGIC(items);
     if (!SvROK(items) || SvTYPE(SvRV(items)) != SVt_PVAV)
         croak("Data::BloomFilter::Shared->add_many: expected an array reference");
     av = (AV *)SvRV(items);
+    sv_2mortal(SvREFCNT_inc((SV *)av));   /* pin the arrayref: element magic below cannot free it mid-loop */
     top = av_len(av);                     /* last index, -1 if empty */
     {
         STRLEN cnt = (top >= 0) ? (STRLEN)(top + 1) : 0, i;
@@ -154,6 +193,7 @@ add_many(self, items)
         }
         REEXTRACT(self);
         bf_rwlock_wrlock(h);                             /* locked region: NO croak-capable calls */
+        if (h->hdr->sealed) { bf_rwlock_wrunlock(h); croak("Data::BloomFilter::Shared->add_many: filter is frozen (read-only)"); }
         for (i = 0; i < cnt; i++) added += (UV)bf_add_locked(h, ps[i], ls[i]);
         __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);  /* a call always counts, even an empty batch */
         bf_rwlock_wrunlock(h);
@@ -173,9 +213,13 @@ contains(self, item)
   CODE:
     s = SvPVbyte(item, n);                 /* may croak (wide char) -- BEFORE the lock */
     REEXTRACT(self);
-    bf_rwlock_rdlock(h);
-    RETVAL = bf_contains_locked(h, s, n);
-    bf_rwlock_rdunlock(h);
+    if (h->readonly) {                     /* frozen: immutable bits, no lock needed */
+        RETVAL = bf_contains_locked(h, s, n);
+    } else {
+        bf_rwlock_rdlock(h);
+        RETVAL = bf_contains_locked(h, s, n);
+        bf_rwlock_rdunlock(h);
+    }
   OUTPUT:
     RETVAL
 
@@ -186,6 +230,7 @@ merge(self, other)
   PREINIT:
     EXTRACT(self);
   CODE:
+    if (h->readonly) croak("Data::BloomFilter::Shared->merge: filter is frozen (read-only)");
     if (!sv_isobject(other) || !sv_derived_from(other, "Data::BloomFilter::Shared"))
         croak("Data::BloomFilter::Shared->merge: expected a Data::BloomFilter::Shared object");
     BfHandle *o = INT2PTR(BfHandle*, SvIV(SvRV(other)));
@@ -214,11 +259,16 @@ merge(self, other)
     uint64_t *tmp;
     Newx(tmp, (size_t)words, uint64_t);
     SAVEFREEPV(tmp);                 /* freed on normal return OR croak unwind */
-    bf_rwlock_rdlock(o);
-    memcpy(tmp, bf_bits(o), (size_t)words * sizeof(uint64_t));
-    bf_rwlock_rdunlock(o);
+    if (o->readonly) {                     /* frozen other: immutable bits, no lock */
+        memcpy(tmp, bf_bits(o), (size_t)words * sizeof(uint64_t));
+    } else {
+        bf_rwlock_rdlock(o);
+        memcpy(tmp, bf_bits(o), (size_t)words * sizeof(uint64_t));
+        bf_rwlock_rdunlock(o);
+    }
 
     bf_rwlock_wrlock(h);
+    if (h->hdr->sealed) { bf_rwlock_wrunlock(h); croak("Data::BloomFilter::Shared->merge: filter is frozen (read-only)"); }
     bf_merge_words(h, tmp, words);
     __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
     bf_rwlock_wrunlock(h);
@@ -229,10 +279,42 @@ clear(self)
   PREINIT:
     EXTRACT(self);
   CODE:
+    if (h->readonly) croak("Data::BloomFilter::Shared->clear: filter is frozen (read-only)");
     bf_rwlock_wrlock(h);
+    if (h->hdr->sealed) { bf_rwlock_wrunlock(h); croak("Data::BloomFilter::Shared->clear: filter is frozen (read-only)"); }
     bf_clear_locked(h);
     __atomic_fetch_add(&h->hdr->stat_ops, 1, __ATOMIC_RELAXED);
     bf_rwlock_wrunlock(h);
+
+void
+freeze(self)
+    SV *self
+  PREINIT:
+    EXTRACT(self);
+  CODE:
+    if (h->readonly) croak("Data::BloomFilter::Shared->freeze: cannot freeze a read-only handle");
+    if (bf_freeze(h) != 0) croak("Data::BloomFilter::Shared->freeze: msync: %s", strerror(errno));
+    h->readonly = 1;   /* this handle now rejects mutation too (the file is sealed) */
+
+UV
+frozen(self)
+    SV *self
+  PREINIT:
+    EXTRACT(self);
+  CODE:
+    RETVAL = h->hdr->sealed ? 1 : 0;
+  OUTPUT:
+    RETVAL
+
+UV
+readonly(self)
+    SV *self
+  PREINIT:
+    EXTRACT(self);
+  CODE:
+    RETVAL = h->readonly ? 1 : 0;
+  OUTPUT:
+    RETVAL
 
 UV
 capacity(self)
@@ -281,9 +363,13 @@ count(self)
     EXTRACT(self);
     UV n;
   CODE:
-    bf_rwlock_rdlock(h);
-    n = (UV)bf_count_locked(h);
-    bf_rwlock_rdunlock(h);
+    if (h->readonly) {                     /* frozen: immutable bits, no lock */
+        n = (UV)bf_count_locked(h);
+    } else {
+        bf_rwlock_rdlock(h);
+        n = (UV)bf_count_locked(h);
+        bf_rwlock_rdunlock(h);
+    }
     RETVAL = n;
   OUTPUT:
     RETVAL
@@ -300,7 +386,7 @@ stats(self)
         double   fp_rate;
         /* Snapshot under the lock; do all (croak-capable) Perl allocation after
            releasing it -- so an OOM in newHV/newSVuv can never strand the lock. */
-        bf_rwlock_rdlock(h);
+        if (!h->readonly) bf_rwlock_rdlock(h);   /* frozen: immutable, no lock */
         X        = bf_popcount_locked(h);
         n_est    = bf_count_from_popcount(h, X);   /* reuse X -- no second scan */
         m_bits   = h->hdr->m_bits;
@@ -308,7 +394,7 @@ stats(self)
         capacity = h->hdr->capacity;
         fp_rate  = h->hdr->fp_rate;
         ops      = h->hdr->stat_ops;
-        bf_rwlock_rdunlock(h);
+        if (!h->readonly) bf_rwlock_rdunlock(h);
 
         HV *hv = newHV();
         hv_stores(hv, "capacity",   newSVuv((UV)capacity));
@@ -320,6 +406,8 @@ stats(self)
         hv_stores(hv, "fill_ratio", newSVnv((double)X / (double)m_bits));
         hv_stores(hv, "ops",        newSVuv((UV)ops));
         hv_stores(hv, "mmap_size",  newSVuv((UV)h->mmap_size));
+        hv_stores(hv, "frozen",     newSVuv(h->hdr->sealed ? 1 : 0));
+        hv_stores(hv, "readonly",   newSVuv(h->readonly ? 1 : 0));
         RETVAL = newRV_noinc((SV *)hv);
     }
   OUTPUT:
@@ -351,7 +439,7 @@ sync(self)
   PREINIT:
     EXTRACT(self);
   CODE:
-    if (bf_msync(h) != 0) croak("sync: %s", strerror(errno));
+    if (!h->readonly && bf_msync(h) != 0) croak("sync: %s", strerror(errno));
 
 void
 unlink(self, ...)
@@ -359,7 +447,12 @@ unlink(self, ...)
   CODE:
     if (sv_isobject(self) && sv_derived_from(self, "Data::BloomFilter::Shared")) {
         BfHandle *h = INT2PTR(BfHandle*, SvIV(SvRV(self)));
-        if (h && h->path) unlink(h->path);
+        if (h && h->path && unlink(h->path) != 0 && errno != ENOENT)
+            croak("Data::BloomFilter::Shared->unlink(%s): %s", h->path, strerror(errno));
     } else if (items >= 2 && (SvGETMAGIC(ST(1)), SvOK(ST(1)))) {
-        unlink(SvPV_nolen(ST(1)));
+        {
+            const char *up = SvPV_nolen(ST(1));
+            if (unlink(up) != 0 && errno != ENOENT)
+                croak("Data::BloomFilter::Shared->unlink(%s): %s", up, strerror(errno));
+        }
     }

vividsnow/perl5-data-bitset-shared

Shared.xs
@@ -10,7 +10,24 @@
         croak("Expected a Data::BitSet::Shared object"); \
     BsHandle *h = INT2PTR(BsHandle*, SvIV(SvRV(sv))); \
     if (!h) croak("Attempted to use a destroyed Data::BitSet::Shared object"); \
+    BsHandle *h0 = h; PERL_UNUSED_VAR(h0); \
     sv_2mortal(SvREFCNT_inc(SvRV(sv)))
+
+/* Re-read the handle after a call that can run Perl code. EXTRACT_BS's
+ * sv_2mortal(SvREFCNT_inc(...)) pin only blocks REFCOUNT-driven destruction;
+ * an explicit $obj->DESTROY frees the handle regardless and zeroes the IV.
+ * The same Perl can also REPLACE the invocant ($obj = 42 mutates ST(0),
+ * because Perl passes aliases), hence the SvROK re-check.
+ *
+ * No instance method needs it today: every index/value argument is a plain
+ * typemap UV, which xsubpp converts in INPUT, BEFORE PREINIT's EXTRACT_BS
+ * runs. It is defined for the method that eventually converts an SV after
+ * extracting the handle. */
+#define REEXTRACT_BS(sv) \
+    if (!SvROK(sv)) \
+        croak("Data::BitSet::Shared object was replaced during the call"); \
+    h = INT2PTR(BsHandle*, SvIV(SvRV(sv))); \
+    if (h != h0) croak("Data::BitSet::Shared object replaced or destroyed during the call")
 
 #define MAKE_OBJ(class, handle) \
     SV *obj = newSViv(PTR2IV(handle)); \
@@ -37,7 +54,7 @@ new(class, path, capacity, ...)
     mode_t mode = (items > 3 && (SvGETMAGIC(ST(3)), SvOK(ST(3)))) ? (mode_t)SvUV(ST(3)) : 0600;
     const char *p = (SvGETMAGIC(path), SvOK(path)) ? SvPV_nolen(path) : NULL;
     BsHandle *h = bs_create(p, capacity, mode, errbuf);
-    if (!h) croak("Data::BitSet::Shared->new: %s", errbuf);
+    if (!h) croak("Data::BitSet::Shared->new: %s", errbuf[0] ? errbuf : "out of memory");
     /* Re-read the class PV at the point of use: xsubpp captured it in INPUT,
      * before the argument magic above ran, and that magic can realloc/free
      * the PV, leaving MAKE_OBJ to bless into a stale (or reused) buffer.
@@ -60,7 +77,7 @@ new_memfd(class, name, capacity)
      * i.e. before SvUV(ST(2)) get-magic, which could realloc/free that PV. */
     const char *nm = (SvGETMAGIC(name), SvOK(name)) ? SvPV_nolen(name) : NULL;
     BsHandle *h = bs_create_memfd(nm, capacity, errbuf);
-    if (!h) croak("Data::BitSet::Shared->new_memfd: %s", errbuf);
+    if (!h) croak("Data::BitSet::Shared->new_memfd: %s", errbuf[0] ? errbuf : "out of memory");
     /* Re-read the class PV at the point of use (see new() above): capacity's
      * INPUT conversion and the name magic both ran after xsubpp captured
      * class. */
@@ -77,9 +94,33 @@ new_from_fd(class, fd)
     char errbuf[BS_ERR_BUFLEN];
   CODE:
     BsHandle *h = bs_open_fd(fd, errbuf);
-    if (!h) croak("Data::BitSet::Shared->new_from_fd: %s", errbuf);
+    if (!h) croak("Data::BitSet::Shared->new_from_fd: %s", errbuf[0] ? errbuf : "out of memory");
     /* Re-read the class PV at the point of use (see new() above): fd's INPUT
      * conversion ran get-magic after xsubpp captured class. */
+    class = SvPV_nolen(ST(0));
+    MAKE_OBJ(class, h);
+  OUTPUT:
+    RETVAL
+
+SV *
+new_readonly(class, path)
+    const char *class
+    SV *path
+  PREINIT:
+    char errbuf[BS_ERR_BUFLEN];
+  CODE:
+    /* Open a FROZEN (sealed) file read-only: O_RDONLY + PROT_READ, no lock
+       ever.  A sealed file is immutable and no read path writes the mapping,
+       so queries take no reader-slot / rwlock traffic and any number of
+       processes can share one PROT_READ mapping (same architecture; the
+       magic rejects a wrong-endian file). */
+    const char *p = (SvGETMAGIC(path), SvOK(path)) ? SvPV_nolen(path) : NULL;
+    if (!p) croak("Data::BitSet::Shared->new_readonly: path is required");
+    BsHandle *h = bs_open_readonly(p, errbuf);
+    if (!h) croak("Data::BitSet::Shared->new_readonly: %s", errbuf);
+    /* Re-read the class PV at the point of use (see new() above): path's
+     * get-magic above could have run Perl code that reallocs/frees it.
+     * BitSet has no REREAD_CLASS macro -- inlined here as elsewhere in this file. */
     class = SvPV_nolen(ST(0));
     MAKE_OBJ(class, h);
   OUTPUT:
@@ -89,7 +130,7 @@ void
 DESTROY(self)
     SV *self
   CODE:
-    if (!SvROK(self)) return;
+    if (!sv_isobject(self) || !sv_derived_from(self, "Data::BitSet::Shared")) return;
     BsHandle *h = INT2PTR(BsHandle*, SvIV(SvRV(self)));
     if (!h) return;
     sv_setiv(SvRV(self), 0);
@@ -114,7 +155,14 @@ set(self, bit)
   PREINIT:
     EXTRACT_BS(self);
   CODE:
+    if (h->readonly) croak("Data::BitSet::Shared->set: bitset is frozen (read-only)");
     CHECK_BIT(h, bit);
+    /* Re-check the live shared header immediately before the CAS: BitSet has no
+     * rwlock to serialize against a PEER process calling ->freeze between our
+     * readonly check and this write, so this atomic load is the closest
+     * available analog to the rest of the family's post-wrlock sealed re-check. */
+    if (__atomic_load_n(&h->hdr->sealed, __ATOMIC_ACQUIRE))
+        croak("Data::BitSet::Shared->set: bitset is frozen (read-only)");
     RETVAL = bs_set(h, bit);
   OUTPUT:
     RETVAL
@@ -126,7 +174,10 @@ clear(self, bit)
   PREINIT:
     EXTRACT_BS(self);
   CODE:
+    if (h->readonly) croak("Data::BitSet::Shared->clear: bitset is frozen (read-only)");
     CHECK_BIT(h, bit);
+    if (__atomic_load_n(&h->hdr->sealed, __ATOMIC_ACQUIRE))
+        croak("Data::BitSet::Shared->clear: bitset is frozen (read-only)");
     RETVAL = bs_clear(h, bit);
   OUTPUT:
     RETVAL
@@ -138,7 +189,10 @@ toggle(self, bit)
   PREINIT:
     EXTRACT_BS(self);
   CODE:
+    if (h->readonly) croak("Data::BitSet::Shared->toggle: bitset is frozen (read-only)");
     CHECK_BIT(h, bit);
+    if (__atomic_load_n(&h->hdr->sealed, __ATOMIC_ACQUIRE))
+        croak("Data::BitSet::Shared->toggle: bitset is frozen (read-only)");
     RETVAL = bs_toggle(h, bit);
   OUTPUT:
     RETVAL
@@ -189,6 +243,9 @@ fill(self)
   PREINIT:
     EXTRACT_BS(self);
   CODE:
+    if (h->readonly) croak("Data::BitSet::Shared->fill: bitset is frozen (read-only)");
+    if (__atomic_load_n(&h->hdr->sealed, __ATOMIC_ACQUIRE))
+        croak("Data::BitSet::Shared->fill: bitset is frozen (read-only)");
     bs_fill(h);
 
 void
@@ -197,7 +254,40 @@ zero(self)
   PREINIT:
     EXTRACT_BS(self);
   CODE:
+    if (h->readonly) croak("Data::BitSet::Shared->zero: bitset is frozen (read-only)");
+    if (__atomic_load_n(&h->hdr->sealed, __ATOMIC_ACQUIRE))
+        croak("Data::BitSet::Shared->zero: bitset is frozen (read-only)");
     bs_zero(h);
+
+void
+freeze(self)
+    SV *self
+  PREINIT:
+    EXTRACT_BS(self);
+  CODE:
+    if (h->readonly) croak("Data::BitSet::Shared->freeze: cannot freeze a read-only handle");
+    if (bs_freeze(h) != 0) croak("Data::BitSet::Shared->freeze: msync: %s", strerror(errno));
+    h->readonly = 1;   /* this handle now rejects mutation too (the file is sealed) */
+
+UV
+frozen(self)
+    SV *self
+  PREINIT:
+    EXTRACT_BS(self);
+  CODE:
+    RETVAL = __atomic_load_n(&h->hdr->sealed, __ATOMIC_ACQUIRE) ? 1 : 0;
+  OUTPUT:
+    RETVAL
+
+UV
+readonly(self)
+    SV *self
+  PREINIT:
+    EXTRACT_BS(self);
+  CODE:
+    RETVAL = h->readonly ? 1 : 0;
+  OUTPUT:
+    RETVAL
 
 SV *
 first_set(self)
@@ -247,7 +337,7 @@ sync(self)
   PREINIT:
     EXTRACT_BS(self);
   CODE:
-    if (bs_msync(h) != 0) croak("msync: %s", strerror(errno));
+    if (!h->readonly && bs_msync(h) != 0) croak("msync: %s", strerror(errno));
 
 void
 unlink(self_or_class, ...)
@@ -263,7 +353,7 @@ unlink(self_or_class, ...)
         p = SvPV_nolen(ST(1));
     }
     if (!p) croak("cannot unlink anonymous or memfd object");
-    if (unlink(p) != 0) croak("unlink(%s): %s", p, strerror(errno));
+    if (unlink(p) != 0 && errno != ENOENT) croak("unlink(%s): %s", p, strerror(errno));
 
 SV *
 stats(self)
@@ -278,6 +368,8 @@ stats(self)
     hv_store(hv, "clears", 6, newSVuv((UV)__atomic_load_n(&h->hdr->stat_clears, __ATOMIC_RELAXED)), 0);
     hv_store(hv, "toggles", 7, newSVuv((UV)__atomic_load_n(&h->hdr->stat_toggles, __ATOMIC_RELAXED)), 0);
     hv_store(hv, "mmap_size", 9, newSVuv((UV)h->mmap_size), 0);
+    hv_store(hv, "frozen", 6, newSVuv(__atomic_load_n(&h->hdr->sealed, __ATOMIC_ACQUIRE) ? 1 : 0), 0);
+    hv_store(hv, "readonly", 8, newSVuv(h->readonly ? 1 : 0), 0);
     RETVAL = newRV_noinc((SV *)hv);
   OUTPUT:
     RETVAL